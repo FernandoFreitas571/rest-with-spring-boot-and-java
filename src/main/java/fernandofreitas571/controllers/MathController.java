208c8a861a7d396638c4db3ffcfd98a4c0676d99
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-25 23:05:16 -0300

    Tratand Exceções com @ControllerAdvice, @ResponseStatus e @ExceptionHandler

---
+++ src/main/java/fernandofreitas571/controllers/MathController.java
@@ -0,0 +1,38 @@
+package fernandofreitas571.controllers;
+
+import fernandofreitas571.exception.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    // http://localhost:8080/math/sum/3/5
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo) {
+        if(!isNumetic(numberOne) || !isNumetic(numberTwo)) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    private boolean isNumetic(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
+        return (number.matches("[+-]?[0-9]*\\.?[0-9]+"));
+    }
+
+    // http://localhost:8080/math/subtraction/3/5
+
+    // http://localhost:8080/math/division/3/5
+
+
+}
---
+++ src/main/java/fernandofreitas571/exception/ExceptionResponse.java
@@ -0,0 +1,6 @@
+package fernandofreitas571.exception;
+
+import java.util.Date;
+
+public record ExceptionResponse(Date timestamp, String message, String details) {
+}
